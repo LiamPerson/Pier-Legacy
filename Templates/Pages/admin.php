ea902eb3635ef54063feaf3ede05817618845a34
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2020-06-14 12:58:13 +1000

    added admin panel

--- Templates/Pages/admin.php
+++ Templates/Pages/admin.php
@@ -3,4 +3,7 @@
         <a class="btn btn-primary" href="/admin/add-movie">Add Movie</a>
         <a class="btn btn-success" href="/admin/movie-list">Edit Movies</a>
     </div>
+    <div class="col">
+        <a class="btn btn-info" href="/admin/list-files-in-directory?directory=stored">List Files in Directory</a>
+    </div>
 </div>
---
+++ Templates/Pages/admin/list-files-in-directory.php
@@ -0,0 +1,59 @@
+<?php
+// Get files from directory specified
+$dir = ROOT . $_GET["directory"];
+$rootLength = strlen(ROOT);
+$localDir = substr($dir, $rootLength);
+?>
+
+<!--<form action="" method="get">-->
+<!--    <label for="listFilesFromX">List Files From</label>-->
+<!--    <input id="listFilesFromX" type="text" name="directory" class="form-control" placeholder="Directory to search">-->
+<!--    <button type="submit" class="btn btn-primary">Search</button>-->
+<!--</form>-->
+<div class="list-group mt-3">
+
+    <?php
+    // Make back button
+    if (!empty($localDir) && $dir != ROOT && $_GET["directory"] != "stored") {
+        $finalSlash = strripos($localDir, '/');
+        $backOneDirectory = substr_replace($localDir, "", $finalSlash);
+        echo '<a class="list-group-item list-group-item-action" href="/admin/list-files-in-directory?directory=' . $backOneDirectory . '"><i class="fas fa-arrow-left"></i> Back</a>';
+    }
+
+
+    // Each item
+    $items = scandir($dir);
+    foreach ($items as $item) {
+        if ($item == '.' || $item == "..")
+            continue;
+
+        $extensionLocation = strripos($item, ".");
+        if ($extensionLocation) {
+            // Non-folders or directories
+            $href = '/' . $localDir . "/" . $item;
+            $icon = '<i class="fas fa-file"></i>';
+            switch (substr($item, $extensionLocation)) {
+                // Videos
+                case ".mp4":
+                case ".webm":
+                    $icon = '<i class="fas fa-play-circle"></i>';
+                    break;
+
+                // Audio
+                case ".mp3":
+                case ".wav":
+                case ".ogg":
+                    $icon = '<i class="fas fa-volume-up"></i>';
+                    break;
+
+            }
+            echo '<a class="list-group-item item-link list-group-item-action" href="' . $href . '">' . $icon . ' ' . $item . '</a>';
+        } else {
+            // Folders / Directories
+            echo '<a class="list-group-item item-link list-group-item-action" href="/admin/list-files-in-directory?directory=' . $localDir . "%2F" . $item . '"><i class="fas fa-folder"></i> ' . $item . '</a>';
+        }
+
+
+    }
+    ?>
+</div>
